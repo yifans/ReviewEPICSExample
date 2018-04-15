32a4b6c929aae5d2f1ecf88e8b85022c950b868e
Yifan Song <yifans@mail.ustc.edu.cn>
2018-04-15 12:52:27 +0800

    建立example ioc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReviewEPICSExample
 Review the examples of EPICS IOC
+
+EPICS Base中提供了多个example，如exampleApp，caClient等，可以用来了解 EPICS Application的开发过程。
---
+++ exampleApp/example-clean/exampleApp/src/devexampleVersion.c
@@ -0,0 +1,38 @@
+/* devexampleVersion.c */
+/* Example device support for the lsi (long string input) record
+ * providing the module version string as the value
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "devSup.h"
+#include "lsiRecord.h"
+
+#include "exampleVersion.h"
+
+/* must be last include */
+#include "epicsExport.h"
+
+const char const version[] = exampleVERSION;
+
+static long read_string(lsiRecord *prec)
+{
+    size_t N = sizeof version;
+    char *buf = prec->val;
+
+    if (N > prec->sizv)
+        N = prec->sizv;
+    prec->len = N;
+
+    memcpy(buf, version, N);
+    buf[N - 1] = '\0';
+
+    return 0;
+}
+
+static lsidset devexampleVersion = {
+    5, NULL, NULL, NULL, NULL, read_string
+};
+epicsExportAddress(dset,devexampleVersion);
